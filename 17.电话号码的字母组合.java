c123b3c8317d060a17528e3710eb182e0eeb7ec2
ZALA <3055853351@qq.com>
2025-12-14 22:49:35 +0800

    feat/11,15

---
+++ 17.电话号码的字母组合.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=17 lang=java
+ * @lcpr version=30305
+ *
+ * [17] 电话号码的字母组合
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "23"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "2"\n
+// @lcpr case=end
+
+ */
+
---
+++ 206.反转链表.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=java
+ * @lcpr version=30305
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+ */
+
