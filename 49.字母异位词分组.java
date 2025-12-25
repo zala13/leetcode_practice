cc4e58aa846f88790bfcbe4359c5147146e1e755
zala13 <124238203+zala13@users.noreply.github.com>
2025-12-25 23:39:44 +0800

    feat/49

--- 49.字母异位词分组.java
+++ 49.字母异位词分组.java
@@ -6,9 +6,34 @@
  */
 
 // @lc code=start
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
+        HashMap<String, List<String>> map = new HashMap();
+        for (int i = 0; i < strs.length; i++) {
+            String key = encode(strs[i]);
+            map.putIfAbsent(key, new LinkedList<>());
+            map.get(key).add(strs[i]);
+        }
+
+        List<List<String>> res = new LinkedList<>();
+        for (List<String> val : map.values()) {
+            res.add(val);
+        }
+        return res;
+    }
+
+    private String encode(String str) {
+        char[] encoder = new char[26];
+        for (int i = 0; i < str.length(); i++) {
+            int n = str.charAt(i) - 'a';
+            encoder[n]++;
+        }
+        return new String(encoder);
     }
 }
 // @lc code=end
